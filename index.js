2d73750d11b978d47efe7174035c44ec56b43f42
Ngaremaina <ngaremaina4@gmail.com>
2023-03-31 13:31:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-  for (let i = 0; i < array.length; i++) {
-    let complement = target - array[i];
-    for (let j = i + 1; j < array.length; j++) {  
-      if (array[j] === complement) {
-        return true
-      };
-    }
+  // for (let i = 0; i < array.length; i++) {
+  //   let complement = target - array[i];
+  //   for (let j = i + 1; j < array.length; j++) {  
+  //     if (array[j] === complement) {
+  //       return true
+  //     };
+  //   }
+  // }
+  // return false;
+  const seenNumbers = {}
+  for (const num of array){
+    let complement = target - num
+    if (seenNumbers[complement]) return true
+    seenNumbers[num] = true
+
   }
-  return false;
+  return false
 }
 
 /* 
